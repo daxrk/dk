57734378ef5aac445e150723124e12439d6d9bac
Mostafa Mohamed <77597243+daxrk@users.noreply.github.com>
2023-01-19 03:57:49 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+$(function () {
+$('#MOSTAFA,#mostafa').addClass("active");
+$('.youtube,.twitter').addClass('active2');
+$('.facebook,.saraha').addClass('active3');
+$('.instagram,.tiktok').addClass('active4');
+$('#mostafa span').animate({
+  width: "189px"
+},2000).animate({
+  borderRight: "0"
+},200);
+
+})
+
+
+function myFunction() {
+    /* Get the text field */
+    var copyText = document.getElementById("myInput");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.text());
+    
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.text());
+  }
